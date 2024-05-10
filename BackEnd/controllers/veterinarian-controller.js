5accbcbdde7d46bfb615cf7d099c1945abc5ebb0
ArtunKARA <artunankara@gmail.com>
2024-05-10 15:32:51 +0300

    veterian

--- BackEnd/controllers/veterinarian-controller.js
+++ BackEnd/controllers/veterinarian-controller.js
@@ -19,6 +19,24 @@ exports.getFarmerVeterinarianData = async (req, res) => {
     }
 };
 
+exports.getAllVeterinarianRequests = async (req, res) => {
+  q = `SELECT fr.ID, fr.status, fr.requestDate, fr.diagnosis, fr.situation, u.farmAdres
+  FROM veterinarianRequests fr
+  JOIN Users u ON fr.userID = u.ID
+  WHERE fr.status = 'a' AND fr.requestResponsible IS NULL;`;
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query(q);
+    if (result.recordset.length === 0) {
+      return res.status(404).json({ error: "User data not found." });
+    }
+    return res.json(result.recordset);
+  } catch (error) {
+    console.error('User data could not be retrieved:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
 exports.getVeterinarianVeterinarianData = async (req, res) => {
   const mail = req.params.mail;
   q = `SELECT fr.ID, fr.status, fr.requestDate, fr.diagnosis, fr.situation, u.farmAdres
@@ -39,14 +57,11 @@ exports.getVeterinarianVeterinarianData = async (req, res) => {
 };
 
 exports.getVeterinarianDataDetails = async (req, res) => {
-
-};
-
-exports.getAllVeterinarianRequests = async (req, res) => {
+  const id = req.params.id;
   q = `SELECT fr.ID, fr.status, fr.requestDate, fr.diagnosis, fr.situation, u.farmAdres
   FROM veterinarianRequests fr
   JOIN Users u ON fr.userID = u.ID
-  WHERE fr.status = 'a' AND fr.requestResponsible IS NULL;`;
+  WHERE fr.ID = '`+id+`';`;
   try {
     const pool = await poolPromise;
     const result = await pool.request().query(q);
@@ -58,7 +73,7 @@ exports.getAllVeterinarianRequests = async (req, res) => {
     console.error('User data could not be retrieved:', error);
     return res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
 
 exports.setFarmerVeterinarianRequest = async (req, res) => {
 
@@ -79,10 +94,39 @@ exports.setFarmerVeterinarianRequest = async (req, res) => {
 };
 
 exports.setVeterinarianRequestAproved = async (req, res) => {
-
+  const { id, mail } = req.body;
+  q = `UPDATE veterinarianRequests SET requestResponsible = (SELECT ID FROM Users WHERE mail = '`+mail+`'), status = 'a' WHERE ID = '`+id+`';`;
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query(q);
+    return res.json({ message: "Request approved successfully" });
+  } catch (error) {
+    console.error('Request could not be approved:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 exports.setVeterinarianRequestDiagnosis = async (req, res) => {
-
+  const { id, diagnosis } = req.body;
+  q = `UPDATE veterinarianRequests SET diagnosis = '`+diagnosis+`' , status = 'p' WHERE ID = '`+id+`';`;
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query(q);
+    return res.json({ message: "Diagnosis set successfully" });
+  } catch (error) {
+    console.error('Diagnosis could not be set:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
+exports.setVeterinarianRequestCanceled = async (req, res) => {
+  q = `UPDATE veterinarianRequests SET status = 'p' WHERE ID = '`+req.params.id+`';`;
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query(q);
+    return res.json({ message: "Request canceled successfully" });
+  } catch (error) {
+    console.error('Request could not be canceled:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
--- BackEnd/routes/veterinarian.js
+++ BackEnd/routes/veterinarian.js
@@ -3,23 +3,24 @@ const router = express.Router();
 
 const {
   getFarmerVeterinarianData,
-  getAllVeterinarianRequests, // tüm veteriner isteklerini getirir
+  getAllVeterinarianRequests,
   getVeterinarianVeterinarianData,
   getVeterinarianDataDetails,
   setFarmerVeterinarianRequest,
   setVeterinarianRequestAproved,
   setVeterinarianRequestDiagnosis,
+  setVeterinarianRequestCanceled,
 } = require("../controllers/veterinarian-controller.js");
 
-// router.get("/situation", veterinarianRequestsSituation);
-// router.get("/status", getVeterinarianRequestsStatus);
+
 router.get("/farmer/:mail", getFarmerVeterinarianData); // Çiftiçinin veriseine göre seriner istekleri getirir  (urlde mail bilgisi olmalı)
-router.get("/all", getAllVeterinarianRequests); // Tüm veteriner isteklerini getirir
-// yazılması lazım router.post("/veterinarian", getVeterinarianVeterinarianData); // Veterinerin veriseine göre seriner istekleri getirir  (urlde mail bilgisi olmalı)
-router.get("/details", getVeterinarianDataDetails);
+router.get("/all", getAllVeterinarianRequests); // Tüm veteriner isteklerini getirir veteriner üstüne alımmamış olanlar
+router.get("/veterinarian/:mail", getVeterinarianVeterinarianData); // Veterinerin veriseine göre seriner istekleri getirir  (urlde mail bilgisi olmalı)
+router.get("/details/:id", getVeterinarianDataDetails); // Veteriner isteğinin detaylarını getirir (urlde talep id bilgisi olmalı)
 router.post("/farmerRequest", setFarmerVeterinarianRequest); // Çiftçinin veteriner isteği oluşturmasını sağlar (bodyde mail ve status bilgisi olmalı)
-router.post("/veterinarianRequestAproved", setVeterinarianRequestAproved);
-router.post("/veterinarianRequestDiagnosis", setVeterinarianRequestDiagnosis);
+router.post("/veterinarianRequestAproved", setVeterinarianRequestAproved); // Veteriner isteğini onaylar (bodyde talep id bilgisi ve onaycı mail bilgisi olmalı)
+router.post("/veterinarianRequestDiagnosis", setVeterinarianRequestDiagnosis); // Veteriner isteğine tanı koyar (bodyde talep id ve tanı bilgisi olmalı)
+router.get("/veterinarianRequestCanceled/:id", setVeterinarianRequestCanceled); // Veteriner isteğini iptal eder (urlde talep id bilgisi olmalı)
 
 
 module.exports = router;
